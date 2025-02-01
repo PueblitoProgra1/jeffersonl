61b794b0364bc612f228a2a03392246cc681951f
COMPUTO_07 PUEBLITO <jr120720241@gmail.com>
2025-02-01 15:30:42 -0600

    primer ejercicio

---
+++ Declaraciones_operadores/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+      double a =5;
+      double b; //under find
+        b= 7;
+        double c =b/a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+
+
+       }
+}
